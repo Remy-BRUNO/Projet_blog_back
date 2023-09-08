2296918cc12383263506f2f738ef0e4e6e91ae89
Reiggnar <bruno.remy.sebastien@gmail.com>
2023-09-08 09:59:02 +0200

    deployd v2

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg")
 
-const pool = new Pool({ ssl: { rejectUnauthorized: false } })
+const pool = new Pool({ ssl: process.env.NODE_ENV === "production" })
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
